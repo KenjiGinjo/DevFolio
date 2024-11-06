fd9202a24c697e7ace6138e9b3d24e0589501d9e
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-11-07 04:17:20 +0800

    Remove unused language list from settings and simplify language handling

--- packages/devfolio-web/dictionaries/settings.ts
+++ packages/devfolio-web/dictionaries/settings.ts
@@ -2,16 +2,5 @@ import { EnumLanguage } from '@devfolio/enum'
 
 export const fallbackLng = EnumLanguage.US
 export const languages = [fallbackLng, EnumLanguage.CN] as const
-export const languageList = [{
-  alias: 'En',
-  value: fallbackLng,
-  path: `${process.env.NEXT_PUBLIC_URL}`,
-}, {
-  alias: '简',
-  value: EnumLanguage.CN,
-  path: `${process.env.NEXT_PUBLIC_URL}/zh-CN`,
-}]
 
 export type Language = (typeof languages)[number]
-
-export const langCookieName = 'i18next'
